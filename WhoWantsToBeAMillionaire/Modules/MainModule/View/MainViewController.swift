051d880f34844bc788c7da972c488c6929e4ddd5
Demogorgonius <51359795+Demogorgonius@users.noreply.github.com>
2024-02-26 00:15:23 +0300

    Starting commit
    add gitignore

---
+++ WhoWantsToBeAMillionaire/Modules/MainModule/View/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  WhoWantsToBeAMillionaire
+//
+//  Created by Sergey on 26.02.2024.
+//
+
+import Foundation
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+}

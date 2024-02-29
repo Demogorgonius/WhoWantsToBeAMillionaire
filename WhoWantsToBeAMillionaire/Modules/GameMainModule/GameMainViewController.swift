e6c17827b2e2cdaf8fa9c071ada7881a72ad939d
Maksim Stogniy <mstogniy@yandex.com>
2024-02-29 19:55:44 +0300

    Add game view

---
+++ WhoWantsToBeAMillionaire/Modules/GameMainModule/GameMainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  GameScreenViewController.swift
+//  WhoWantsToBeAMillionaire
+//
+//  Created by  Maksim Stogniy on 29.02.2024.
+//
+
+import UIKit
+
+final class GameMainViewController: CustomViewController<GameMainView> {
+
+    //MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ WhoWantsToBeAMillionaire/Modules/GameMainModule/View/ClueButtonView.swift
@@ -0,0 +1,53 @@
+//
+//  ClueButtonView.swift
+//  WhoWantsToBeAMillionaire
+//
+//  Created by  Maksim Stogniy on 29.02.2024.
+//
+
+import UIKit
+import SnapKit
+
+final class ClueButtonView: UIView {
+    
+    private var button: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .clear
+        button.contentMode = .scaleAspectFill
+        button.configuration?.title = ""
+        
+        return button
+    }()
+
+    // MARK: Init
+    init(image: UIImage) {
+        self.button.setImage(image, for: .normal)
+        super.init(frame: .zero)
+        setViews()
+        layoutViews()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func addTarget(selector: Selector) {
+        button.addTarget(self, action: selector, for: .touchUpInside)
+    }
+}
+
+// MARK: - Setup Veiws
+extension ClueButtonView {
+
+    private func setViews() {
+        addSubview(button)
+    }
+
+    private func layoutViews() {
+        button.snp.makeConstraints { make in
+            make.bottom.top.left.right.equalToSuperview()
+            make.height.equalTo(80)
+            make.width.equalTo(100)
+        }
+    }
+}

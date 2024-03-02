462825e922eb5fd00682073fb016c32d7dbb8cbc
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-03-03 02:45:48 +0300

    add playsound & popview method

--- WhoWantsToBeAMillionaire/Modules/QustionsModule/QuestionsViewController.swift
+++ WhoWantsToBeAMillionaire/Modules/QustionsModule/QuestionsViewController.swift
@@ -9,7 +9,23 @@ import UIKit
 
 final class QuestionsViewController: CustomViewController<QuestionsView> {
     
-    let musicService = GameMusicService()
+    private let musicService = GameMusicService()
+    
+    /// индекс текущего вопроса
+    private var questionIndex: Int = 0
+    /// выбор проигрываемой мелодии в зависимости от правильности ответа
+    private var isCorrect: Bool
+    
+    // MARK: Init
+    init(number: Int, isCorrect: Bool) {
+        self.questionIndex = number
+        self.isCorrect = isCorrect
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     // MARK: Life Cycle
     override func viewDidLoad() {
@@ -21,4 +37,23 @@ final class QuestionsViewController: CustomViewController<QuestionsView> {
         musicService.userWin()
     }
     
+    // Запуск музыки зависимо от значения true/false
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        playSound()
+        popView()
+    }
+    
+    // Активация музыки.
+    private func playSound() {
+        isCorrect ? musicService.rightAnswer() : musicService.wrongAnswer()
+    }
+    
+    /// возврат на предыдущий экран по истечении 3 секунд
+    private func popView() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+            self.musicService.stopPlaying()
+            self.navigationController?.popViewController(animated: true)
+        }
+    }
 }

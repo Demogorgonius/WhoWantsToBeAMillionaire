2e7f1bec03a951da796ad557d003fcb88adfba2e
Maksim Stogniy <mstogniy@yandex.com>
2024-03-03 12:53:30 +0300

    Update gameover screen

--- WhoWantsToBeAMillionaire/Modules/GameOverModule/View/GameOverViewController.swift
+++ WhoWantsToBeAMillionaire/Modules/GameOverModule/View/GameOverViewController.swift
@@ -9,16 +9,68 @@ import UIKit
 
 final class GameOverViewController: CustomViewController<GameOverView> {
     
+    
+    /// индекс текущего вопроса
+    private var questionIndex: Int = 0
+    private var amount: Int = 0
+
+    private var progressType: ProgressType
+    
+    private let registrationService = RegistrationUserService()
+    
     //MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        commonInit()
+        customView.delegate = self
     }
     
-    private func commonInit() {
-        customView.delegate = self
-        customView.configure(attemptsCount: 3)
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        setLabels()
+        registrationService.setAmountForLastUser(amount)
+    }
+    // MARK: Init
+    init(number: Int, progressType: ProgressType, amount: Int) {
+        self.questionIndex = number
+        self.progressType = progressType
+        self.amount = amount
+       
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    func setLabels() {
+        let questionString = "вопрос\(getEndingByNumber())"
+        switch progressType {
+        case .win:
+            customView.setLoseTitleLabel("Выигрыш: \(amount) RUB")
+            customView.setAttemptTitleLabelText("Вы ответили на 15 \(questionString)")
+        case .loose:
+            customView.setLoseTitleLabel("Проиграли: \(amount) RUB")
+            customView.setAttemptTitleLabelText("Вы ответили на \(questionIndex) \(questionString)")
+        case .getMoney:
+            customView.setLoseTitleLabel("Вы забрали деньги: \(amount) RUB")
+            customView.setAttemptTitleLabelText("Вы ответили на \(questionIndex) \(questionString)")
+            break
+        case .newGame:
+            break
+        }
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    func getEndingByNumber() -> String {
+        switch questionIndex {
+        case 1:
+            return ""
+        case 2...4:
+            return "a"
+        default:
+            return "ов"
+        }
     }
 }
 
